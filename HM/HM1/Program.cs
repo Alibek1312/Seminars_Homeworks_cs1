cc74b75b79d6a5069b4fe87ddbed486cf3f9cde2
Alibek1312 <alibek.jamilov@gmail.com>
2023-02-24 15:57:33 +0500

    Did homeworks for checking

---
+++ HM/HM1/Program.cs
@@ -0,0 +1,63 @@
+﻿// ----------------------------------------------------------------------------------------
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Write("Input first number for compare: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number for compare with first: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.Write($"First number {num1} is bigger than Second number {num2}");
+// }
+// else
+// {
+//     Console.Write($"Second number {num2} is bigger than First number {num1}");
+// }
+
+
+// ----------------------------------------------------------------------------------------
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+// Console.Write("Input First number for find max: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input Second number for find max: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input Third number fo find max: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num1 > max) max = num1;
+// if (num2 > max) max = num2;
+// if (num3 > max) max = num3;
+
+// Console.Write($"You input this numbers: {num1}, {num2}, {num3} and Max number is: {max}");
+
+// ----------------------------------------------------------------------------------------
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+// if
+// Console.Write("Input number to check devide in 2: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0)
+// {
+//     Console.Write($"Your input number {num} is deveded in 2");
+// }
+// else
+// {
+//     Console.Write($"Your input number {num} is not deveded in 2");
+// }
+
+// ----------------------------------------------------------------------------------------
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+// while
+Console.Write("Input number and see the result: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int f = 2;
+
+while (f <= num)
+{
+    Console.Write(f + " ");
+    f += 2;
+}
